76c009fb84561a7141a2fd61c8afd453377f1da0
Marti1976 <marti.kos@seznam.cz>
2026-09-01 08:42:14 +0200

    20260901 NEW

---
+++ 3D/blender/search-data.js
@@ -0,0 +1,10 @@
+// Vyhledávací index pro manuál Blender.
+// Obsahuje pouze to, co v manuálu skutečně existuje (zatím jen rozcestník 00).
+// Až vzniknou jednotlivé kapitoly (01_BLE_...html atd.) a reálné odkazy v sekci
+// "Oblíbené odkazy", přidejte je sem.
+
+const SEARCH_INDEX = [
+  { title: "Obsah & Hledání", tags: ["obsah", "rozcestník", "hledání", "kapitoly"], page: "00_BLE_index.html", anchor: "" },
+  { title: "Ostatní přílohy", tags: ["přílohy", "soubory", "vlastní materiály"], page: "00_BLE_index.html", anchor: "#ostatni_prilohy" },
+  { title: "Oblíbené odkazy", tags: ["odkazy", "zdroje", "komunita"], page: "00_BLE_index.html", anchor: "#oblibene_odkazy" }
+];
